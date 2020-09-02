603ca5c2a313c99ebbd51ccecb85c909a3dbbb56
vladrepin26 <revanmax626@gmail.com>
2020-09-02 18:06:38 +0300

    Adding events

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ class App extends Component {
         pageTitle: 'React components'
     }
 
+    changeTitleHandler = () => {
+        console.log('Clicked')
+    }
+
     render() {
         const divStyle = {
             textAlign: 'center'
@@ -23,6 +27,8 @@ class App extends Component {
         return (
             <div style={divStyle}>
                 <h1>{this.state.pageTitle}</h1>
+
+                <button onClick={this.changeTitleHandler}>Change title</button>
 
                 <Car name={cars[0].name} year={cars[0].year}/>
                 <Car name={cars[1].name} year={cars[1].year}/>
