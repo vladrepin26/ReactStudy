8ec8797df4f365e9854214e36501e0d16a6b9891
vladrepin26 <revanmax626@gmail.com>
2020-09-02 17:28:42 +0300

    passing content

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ function App() {
         <div style={divStyle}>
             <h1>Hello</h1>
 
-            <Car name={'Ford'} year={2018} />
-            <Car name={'Audi'} year={2016} />
+            <Car name={'Ford'} year={2018}>
+                <p style={{color: 'blue'}}>Color</p>
+            </Car>
+            <Car name={'Audi'} year={2016}>
+                <p style={{color: 'red'}}>Color</p>
+            </Car>
             <Car name={'Mazda'} year={2010} />
 
         </div>
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -22,5 +22,6 @@ export default (props) => (
     <div>
         <h3>Car name: {props.name}</h3>
         <p>YEAR: <strong>{props.year}</strong></p>
+        { props.children }
     </div>
 )
