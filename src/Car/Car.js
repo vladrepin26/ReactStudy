8d4accba635fcf7016fdbe06ad655559b954fbc5
vladrepin26 <revanmax626@gmail.com>
2020-09-02 17:08:35 +0300

    create simple component

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-function Car() {
-    return (
-        <div>This is car component</div>
-    )
-}
+// function car() {
+//     return (
+//         <div>This is car component</div>
+//     )
+// }
 
-export default Car
+// const car = () => {
+//     return (
+//         <div>This is car component</div>
+//     )
+// }
+
+// const car = () => (
+//     <div>
+//         This is car component
+//         <strong>test</strong>
+//     </div>
+// )
+
+
+export default () => (
+    <div>
+        This is car component
+        <strong>test</strong>
+    </div>
+)
