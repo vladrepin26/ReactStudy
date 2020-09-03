6cffa056b3edff7e2b1bdf83e3a14675202260e0
vladrepin26 <revanmax626@gmail.com>
2020-09-03 13:49:45 +0300

    Сonditional statements_2

--- src/App.js
+++ src/App.js
@@ -30,6 +30,21 @@ class App extends Component {
             textAlign: 'center'
         }
 
+        let cars = null
+
+        if (this.state.showCars) {
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car
+                        key={index}
+                        name={car.name}
+                        year={car.year}
+                        onChangeTitle={() => this.changeTitleHandler(car.name)}
+                    />
+                )
+            })
+        }
+
         return (
             <div style={divStyle}>
                 <h1>{this.state.pageTitle}</h1>
@@ -39,19 +54,7 @@ class App extends Component {
                     Toggle cars
                 </button>
 
-                {this.state.showCars
-                    ? this.state.cars.map((car, index) => {
-                        return (
-                            <Car
-                                key={index}
-                                name={car.name}
-                                year={car.year}
-                                onChangeTitle={() => this.changeTitleHandler(car.name)}
-                            />
-                        )
-                    })
-                    : null
-                }
+                {cars}
             </div>
         );
     }
