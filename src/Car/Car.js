d6668666ea271b258d6c5dec3a576c9c090c4f0a
vladrepin26 <revanmax626@gmail.com>
2020-09-03 15:06:30 +0300

    Add CSS

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,32 +1,9 @@
 import React from "react";
-
-// function car() {
-//     return (
-//         <div>This is car component</div>
-//     )
-// }
-// const car = () => {
-//     return (
-//         <div>This is car component</div>
-//     )
-// }
-// const car = () => (
-//     <div>
-//         This is car component
-//         <strong>test</strong>
-//     </div>
-// )
+import './Car.css'
 
 
 export default (props) => (
-    <div style={{
-        border: '1px solid #ccc',
-        marginBottom: '10px',
-        display: 'block',
-        padding: '10px',
-        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-        borderRadius: '5px',
-    }}>
+    <div className="Car">
         <h3>Car name: {props.name}</h3>
         <p>YEAR: <strong>{props.year}</strong></p>
         <input type="text" onChange={props.onChangeName} value={props.name}/>
